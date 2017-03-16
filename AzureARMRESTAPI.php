37e6b633e40792092140268f9de80349b2deda6a
mschray <mschray@users.noreply.github.com>
2017-03-16 16:06:18 -0500

    Add functions to get deployment operations info

--- AzureARMRESTAPI.php
+++ AzureARMRESTAPI.php
@@ -5,30 +5,40 @@
     print("Load the environment variables\n");
 
     // Load environment variables
-    $is_debug = true;
+
     $dotenv = new Dotenv\Dotenv(__DIR__);
     $dotenv->load();
-    $tenant_id = getenv('TENANT_ID');
-    $client_id = getenv('CLIENT_ID');
-    $client_secret = getenv('CLIENT_SECRET');
-    $subscription_id = getenv('SUBSCRIPTION_ID');
-    $resource_group_name = getenv('RESOURCE_GROUP_NAME');
+    $a_tenant_id = getenv('TENANT_ID');
+    $a_client_id = getenv('CLIENT_ID');
+    $a_client_secret = getenv('CLIENT_SECRET');
+    $a_subscription_id = getenv('SUBSCRIPTION_ID');
+    $a_resource_group_name = getenv('RESOURCE_GROUP_NAME');
+    $azure_datacenter_location = "east us";
 
-    $access_token = get_auth_token($tenant_id, $client_id, $client_secret);
+    $is_debug = true;
 
-    dump_var("Access Token" + $access_token);
+    $access_token = get_auth_token($a_tenant_id, $a_client_id, $a_client_secret);
+    $result = create_resource_group($a_subscription_id,$a_resource_group_name,$access_token,$azure_datacenter_location);
 
-    function make_request($request_array)
+    print("Access token\n");
+    var_dump( $access_token);
+
+    function make_request(array $request_array)
     {
 
         $curl  = curl_init();
         curl_setopt_array($curl, $request_array);
 
-        $resp = json_decode(curl_exec($curl));
+        $result = curl_exec($curl);
+        $resp = json_decode($result);
         
-        if ($is_debug) print("Completed auth request with status of\n");
+        print(curl_error($curl));
 
-        if ($is_debug) var_dump($resp);
+        print("\n");
+
+        print("Completed auth request with status of\n");
+
+        var_dump($resp);
         
         curl_close($curl);
 
@@ -37,59 +47,61 @@
 
     }
 
-    function get_auth_token($a_tenant_id, $a_client_id, $a_client_secret)
+    function get_auth_token($tenant_id, $client_id, $client_secret)
     {
         $curl_opt_array = array(
             CURLOPT_POST => 1,
             CURLOPT_RETURNTRANSFER => 1,
-            CURLOPT_URL => "https://login.microsoftonline.com:443/$a_tenant_id/oauth2/token??api-version=1.0",
+            CURLOPT_URL => "https://login.microsoftonline.com:443/$tenant_id/oauth2/token??api-version=1.0",
             CURLOPT_POSTFIELDS => array(
                 "grant_type" => "client_credentials",
                 "resource" => "https://management.core.windows.net/",
-                "client_id" => "$a_client_id",
-                "client_secret" => "$a_client_secret"
+                "client_id" => "$client_id",
+                "client_secret" => "$client_secret"
             ));
 
 
-        if ($is_debug) print("Dump the $curl_opt_array \n");
-        if ($is_debug) var_dump($curl_opt_array);
+        var_dump($curl_opt_array);
 
-
-        if ($is_debug) print("Make request for auth token\n");
+        print("Make request for auth token\n");
         
         $resp = make_request($curl_opt_array);
         
-        if ($is_debug) var_dump($resp);
+        print_r($resp);
+        var_dump($resp);
 
         $access_token = $resp->access_token;
 
         return $access_token;
     }      
 
+    function create_resource_group($subscription_id,$resource_group_name,$access_token,$azure_location)
+    {
+        /* Create new resource group and deploy from ARM template */
+        $data = array("location" => $azure_location);
+        $data_json_location = json_encode($data);
+        
+        // Initialize curl and set options
+        $curl_opt_array = array(
+            CURLOPT_CUSTOMREQUEST => "PUT",
+            CURLOPT_RETURNTRANSFER => 1,
+            CURLOPT_HEADER => 0,
+            CURLOPT_URL => "https://management.azure.com/subscriptions/$subscription_id/resourcegroups/$resource_group_name?api-version=2016-06-01",
+            CURLOPT_HTTPHEADER => array(
+                "Content-type: Application/json",
+                "Authorization: Bearer $access_token"),
+            CURLOPT_POSTFIELDS => $data_json_location
+        );
 
-    
+        $resp = make_request($curl_opt_array);
+        
+        print_r($resp);
 
-    // /* Create new resource group and deploy from ARM template */
-    // $data = array("location" => "eastus");
-    // $data_json = json_encode($data);
-    // // Initialize curl and set options
-    // $curl = curl_init();
-    // curl_setopt_array($curl, array(
-    //     CURLOPT_CUSTOMREQUEST => "PUT",
-    //     CURLOPT_RETURNTRANSFER => 1,
-    //     CURLOPT_HEADER => 0,
-    //     CURLOPT_URL => "https://management.azure.com/subscriptions/$subscription_id/resourcegroups/$resource_group_name?api-version=2016-06-01",
-    //     CURLOPT_HTTPHEADER => array(
-    //         "Content-type: Application/json",
-    //         "Authorization: Bearer $access_token"),
-    //     CURLOPT_POSTFIELDS => $data_json
-    // ));
+        return $resp;
 
-    // $resp = curl_exec($curl);
-    // print_r($resp);
-    // curl_close($curl);
+    }
 
-    // $deployment_name = "FirstCaylentDeployment3";
+  // $deployment_name = "FirstCaylentDeployment3";
     // $template_uri = "https://raw.githubusercontent.com/Azure/azure-quickstart-templates/master/101-vm-simple-linux/azuredeploy.json";
     // $adminUsername = "KwadwoTest";
     // $adminPassword = "KwadwoTest123!!!";
@@ -118,21 +130,59 @@
     //     )
     // );
     // $data_json = json_encode($data);
-    // // Initialize curl and set options
-    // $curl = curl_init();
-    // $curl = curl_init();
-    // curl_setopt_array($curl, array(
-    //     CURLOPT_CUSTOMREQUEST => "PUT",
-    //     CURLOPT_RETURNTRANSFER => 1,
-    //     CURLOPT_HEADER => 0,
-    //     CURLOPT_URL => "https://management.azure.com/subscriptions/$subscription_id/resourcegroups/$resource_group_name/providers/microsoft.resources/deployments/$deployment_name?api-version=2016-09-01",
-    //     CURLOPT_HTTPHEADER => array(
-    //         "Content-type: Application/json",
-    //         "Authorization: Bearer $access_token"),
-    //     CURLOPT_POSTFIELDS => $data_json
-    // ));
 
-    // $resp = json_decode(curl_exec($curl));
-    // print_r($resp);
-    // curl_close($curl); 
+    function create_or_update_deployment($subscription_id, $resource_group_name, $deployment_name, $data)
+    {
+        $data_json = json_encode($data);
+        // Initialize curl and set options
+        $curl_opt_array = array(
+            CURLOPT_CUSTOMREQUEST => "PUT",
+            CURLOPT_RETURNTRANSFER => 1,
+            CURLOPT_HEADER => 0,
+            CURLOPT_URL => "https://management.azure.com/subscriptions/$subscription_id/resourcegroups/$resource_group_name/providers/microsoft.resources/deployments/$deployment_name?api-version=2016-09-01",
+            CURLOPT_HTTPHEADER => array(
+                "Content-type: Application/json",
+                "Authorization: Bearer $access_token"),
+            CURLOPT_POSTFIELDS => $data_json
+        );
+
+        $resp = make_request($curl_opt_array);
+        
+        print_r($resp);
+
+        return $resp;
+    }
+
+    //https://docs.microsoft.com/en-us/rest/api/resources/deploymentoperations
+    function get_list_deployment_operations()
+    {
+        //https://management.azure.com/subscriptions/$a_subscription_id/resourcegroups/$resource_group_name/deployments/$deployment_name/operations?api-version=2016-09-01
+
+                // Initialize curl and set options
+        $curl_opt_array = array(
+            CURLOPT_CUSTOMREQUEST => "GET",
+            CURLOPT_RETURNTRANSFER => 1,
+            CURLOPT_HEADER => 0,
+            CURLOPT_URL => "https://management.azure.com/subscriptions/$a_subscription_id/resourcegroups/$resource_group_name/deployments/$deployment_name/operations?api-version=2016-09-01",
+            CURLOPT_HTTPHEADER => array(
+                "Content-type: Application/json",
+                "Authorization: Bearer $access_token")
+        );
+
+        $resp = make_request($curl_opt_array);
+        
+        print_r($resp);
+
+        return $resp;
+
+
+    }
+
+    //https://docs.microsoft.com/en-us/rest/api/resources/deploymentoperations
+    function get_deployment_operation()
+    {
+
+    }
+  
+
 ?>
